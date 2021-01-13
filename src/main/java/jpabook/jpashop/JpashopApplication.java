d4bc924c5edeec3dc218820fc311634e00f20459
dhtpdns <yd1505@gmail.com>
2021-01-14 08:00:50 +0900

    member ropository test add

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,12 +1,21 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.origin.SystemEnvironmentOrigin;
 
 @SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
+		Hello hello = Hello.builder()
+				.name("ose")
+				.age(11)
+				.build();
+		hello.getName();
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -10,8 +10,9 @@ import java.util.List;
 public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
-    public void save(Member member) {
+    public Long save(Member member) {
         em.persist(member);
+        return member.getId();
     }
     public Member findOne(Long id) {
         return em.find(Member.class, id);
