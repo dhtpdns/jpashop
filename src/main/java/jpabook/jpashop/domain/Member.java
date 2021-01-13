9f67fe2d07724d1b691fd70b11ad4da64dabc753
dhtpdns <yd1505@gmail.com>
2021-01-13 20:27:40 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+
+}
---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_item")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderItem {
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item; //주문 상품
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order; //주문
+
+    private int orderPrice; //주문 가격
+
+    private int count; //주문 수량
+
+    //==비즈니스 로직==//
+    public static OrderItem creatOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        //제고에서 바로 카운트를 제거하고 넣는다.
+        item.removeStock(count);
+        return orderItem;
+    }
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+    
+    //== 전체 주문가격 ==//
+    /**
+     * 주문상품 전체 가격 조회
+     * @return
+     */
+    public int getTocalPrice() {
+        return getOrderPrice() * getCount();
+    }
+}
